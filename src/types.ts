b00ed161393d3fa8f2f215f165f4d33c2f7207e8
Evert Pot <me@evertpot.com>
2021-06-28 00:36:28 -0400

    Update everything to latest curveball defaults

--- src/types.ts
+++ src/types.ts
@@ -3,16 +3,16 @@ export type Options = {
   /**
    * The location of the assets on the filesystem
    */
-  staticDir: string,
+  staticDir: string;
 
   /**
    * By default the public URL will be prefixed with the basename of the staticDir
    * options. Use this setting to override this
    */
-  pathPrefix?: string,
+  pathPrefix?: string;
 
   /**
    * If specified, all assets will be get a Cache-Control: max-age=n header
    */
-  maxAge?: number,
+  maxAge?: number;
 }
